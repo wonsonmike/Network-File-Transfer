d60b663657ed84a63fcaf250bf4483d2448f42d6
wonsonmike <mwonsoncloud@gmail.com>
2023-10-05 10:26:30 +0300

    First push

---
+++ client.py
@@ -0,0 +1,22 @@
+import socket
+
+def download_file(filename, server_host, server_port):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
+        client_socket.connect((server_host, server_port))
+        client_socket.recv(1024)  # Welcome message
+        
+        command = f"get {filename}"
+        client_socket.sendall(command.encode("utf-8"))
+        
+        response = client_socket.recv(1024).decode("utf-8").strip()
+        if response == "Invalid command. Use 'get filename'.":
+            print(response)
+        else:
+            with open(filename, 'wb') as file:
+                file.write(response.encode())
+
+if __name__ == "__main__":
+    server_host, server_port = "localhost", 2222
+    filename = "sample.txt"  # Replace with the desired filename
+    download_file(filename, server_host, server_port)
+    print(f"File '{filename}' downloaded successfully.")
---
+++ server.py
@@ -0,0 +1,35 @@
+import socketserver
+
+class SFTPServerHandler(socketserver.BaseRequestHandler):
+    def handle(self):
+        self.request.sendall(b"Welcome to the Simple File Sharing Server!\n")
+        
+        while True:
+            self.request.sendall(b"Enter 'get filename' to download a file: ")
+            data = self.request.recv(1024).decode("utf-8").strip()
+            
+            if not data:
+                break
+            
+            command, filename = data.split(maxsplit=1)
+            
+            if command == 'get':
+                self.send_file(filename)
+            else:
+                self.request.sendall(b"Invalid command. Use 'get filename'.\n")
+        
+        self.request.close()
+
+    def send_file(self, filename):
+        try:
+            with open(filename, 'rb') as file:
+                data = file.read()
+                self.request.sendall(data)
+        except FileNotFoundError:
+            self.request.sendall(b"File not found.\n")
+
+if __name__ == "__main__":
+    host, port = "0.0.0.0", 2222
+    server = socketserver.TCPServer((host, port), SFTPServerHandler)
+    print(f"SFTP server is running on {host}:{port}")
+    server.serve_forever()
