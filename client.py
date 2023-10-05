05dc7887ab3c7c0898a6176d9c63db62cb78181d
wonsonmike <mwonsoncloud@gmail.com>
2023-10-05 12:23:51 +0300

    update client and sample file

--- client.py
+++ client.py
@@ -1,22 +1,52 @@
 import socket
 
-def download_file(filename, server_host, server_port):
+def showOptions():
+    print("Here are your options.")
+    print("1) Download a file")
+    print("2) Upload a file")
+    print("3) Delete a file")
+    print("c) Close")
+    return "->"
+
+def download_file(client_socket):
+    print(client_socket.recv(1024).decode("utf-8")) # Give options
+
+    filename = input(">")  # Get with the desired filename from the user
+    command = f"get {filename}"
+    client_socket.sendall(command.encode("utf-8"))
+    
+    response = client_socket.recv(1024).decode("utf-8").strip()
+    if response == "Invalid command. Use 'get filename'.":
+        print(response)
+    elif response == "File not found.":
+        print(response)
+    else:
+        with open(filename, 'wb') as file:
+            file.write(response.encode())
+
+def connect_to_server(server_host, server_port):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
+        #Connect to the client, and confirm connection to the user
         client_socket.connect((server_host, server_port))
-        client_socket.recv(1024)  # Welcome message
-        
-        command = f"get {filename}"
-        client_socket.sendall(command.encode("utf-8"))
-        
-        response = client_socket.recv(1024).decode("utf-8").strip()
-        if response == "Invalid command. Use 'get filename'.":
-            print(response)
-        else:
-            with open(filename, 'wb') as file:
-                file.write(response.encode())
+        print(client_socket.recv(1024).decode("utf-8"))  # Welcome message
+        print(client_socket.recv(1024).decode("utf-8")) # Give options
+
+
+        next = ""
+        while (next != "c"):
+            next = input(showOptions())
+            if next == "1":
+                download_file(client_socket)
+            elif next == "2":
+                print("Upload")
+            elif next == "3":
+                print("Delete")
+            elif next == "c":
+                pass
+            else:
+                print("Command not accepted. Try again.")
+
 
 if __name__ == "__main__":
     server_host, server_port = "192.168.8.135", 2222
-    filename = "sample.txt"  # Replace with the desired filename
-    download_file(filename, server_host, server_port)
-    print(f"File '{filename}' downloaded successfully.")
+    connect_to_server(server_host, server_port)
