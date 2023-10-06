f32eba5484436201f763a800a694454c16596abb
wonsonmike <mwonsoncloud@gmail.com>
2023-10-06 13:53:39 +0300

    fixing formatting, plus add connection close

--- client.py
+++ client.py
@@ -11,20 +11,30 @@ def showOptions():
     return "->"
 
 def list_files(client_socket):
+    # Tell the server to list files
     command = "list"
-    client_socket.sendall(command.encode("utf-8")) # Tell the server to list files
+    client_socket.sendall(command.encode("utf-8")) 
 
+    # Get the list of files from the server and display them
     response = client_socket.recv(4096).decode("utf-8").strip()
     print("List of files in the directory:")
     print(response)
 
 def download_file(client_socket):
+    # Tell the server I want to download a file
     command = "get"
-    client_socket.sendall(command.encode("utf-8")) # Tell the server I want to download a file
+    client_socket.sendall(command.encode("utf-8"))
 
-    filename = input(client_socket.recv(1024).decode("utf-8")) # Print the filename request
-    client_socket.sendall(filename.encode("utf-8")) # Send the filename to the server
+    # List the files to choose from
+    filenames = client_socket.recv(4096).decode("utf-8").strip()
+    print("List of files in the directory:")
+    print(filenames)
+
+    # Get the filename from the user and send it to server
+    filename = input("Enter the filename to download a file: ")
+    client_socket.sendall(filename.encode("utf-8")) 
     
+    # Get the file data (or error message)
     response = b""
     while True: # Loop until response has all data from the file
         data = client_socket.recv(4096)
@@ -36,6 +46,7 @@ def download_file(client_socket):
         else:
             response += data
 
+    # If it's an error, display it. Otherwise, save the file in the files folder
     if response == b"File not found.":
         print(response.decode("utf-8"))
     else:
@@ -44,8 +55,9 @@ def download_file(client_socket):
         print("File successfully downloaded to the files folder.")
 
 def upload_file(client_socket):
+    # Tell the server I want to upload a file
     command = "send"
-    client_socket.sendall(command.encode("utf-8")) # Tell the server I want to upload a file
+    client_socket.sendall(command.encode("utf-8")) 
 
     # List the files from the uploading folder
     print("Select from the following files to upload: ")
@@ -57,7 +69,7 @@ def upload_file(client_socket):
     filename = input("Enter the filename for upload: ")
     client_socket.sendall(filename.encode("utf-8"))
 
-    # Upload the file
+    # Upload the file, or the error message
     try:
         with open("uploading/"+filename, 'rb') as file:
             data = file.read()
@@ -70,13 +82,16 @@ def upload_file(client_socket):
     response = client_socket.recv(1024).decode("utf-8")
     print(response)
 
+def delete_file(client_socket):
+    pass
+
 def connect_to_server(server_host, server_port):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
-        #Connect to the client, and confirm connection to the user
+        # Connect to the client, and confirm connection to the user
         client_socket.connect((server_host, server_port))
         print(client_socket.recv(1024).decode("utf-8"))  # Welcome message
 
-
+        # Get the next option from the user
         next = ""
         while (next != "c"):
             next = input(showOptions())
@@ -87,13 +102,18 @@ def connect_to_server(server_host, server_port):
             elif next == "3":
                 upload_file(client_socket)
             elif next == "4":
-                print("Delete")
+                delete_file(client_socket)
             elif next == "c":
                 pass
             else:
                 print("Command not accepted. Try again.")
 
+        # Close connection with the server
+        command = "close"
+        client_socket.sendall(command.encode("utf-8")) 
+
 
 if __name__ == "__main__":
+    # Connect to the server
     server_host, server_port = "192.168.8.135", 2222
     connect_to_server(server_host, server_port)
--- server.py
+++ server.py
@@ -3,56 +3,73 @@ import os
 
 class SFTPServerHandler(socketserver.BaseRequestHandler):
     def handle(self):
+
         self.request.sendall(b"Welcome to the Simple File Sharing Server!\n")
         
         while True:
+            # Get the command from the client
             command = self.request.recv(1024).decode("utf-8")
 
             if command == 'list': # Client wants to list the files
                 self.list_files()
 
-            elif command == "get": # Clent wants to download a file
+            elif command == "get": # Client wants to download a file
                 self.send_file()
                 
             elif command == "send": # Client wants to upload a file
                 self.get_file()
+
+            elif command == "close": # Client wants to close the connection
+                break
 
             else:
                 self.request.sendall(b"Invalid command.\n")
         
         self.request.close()
 
-    def list_files(self):
+    def list_files(self): # Client wants to list the files
+        # Get the list of files from the files folder
+        files = os.listdir('files')
+        file_list = '\n'.join(files)
+
+        # Send the list of files to the user
+        self.request.sendall(file_list.encode("utf-8"))
+
+    def send_file(self): # Client wants to download a file
+        # Send a list of the files available to download
         files = os.listdir('files')
         file_list = '\n'.join(files)
         self.request.sendall(file_list.encode("utf-8"))
 
-    def send_file(self):
-        self.request.sendall(b"Enter the filename to download a file: ")
+        # Get the filename from the user
         filename = self.request.recv(1024).decode("utf-8").strip()
 
+        # If the file exists, send the data
         try:
             with open("files/"+filename, 'rb') as file:
                 data = file.read()
                 self.request.sendall(data)
-                self.request.sendall(b"===EOF===")
+                self.request.sendall(b"===EOF===") # Clarify this is the end of the file
         except FileNotFoundError:
             self.request.sendall(b"File not found.\n")
 
-    def get_file(self):
+    def get_file(self): # Client wants to upload a file
         # Get the filename from the client
         filename = self.request.recv(1024).decode("utf-8").strip()
 
+        # Get file data (or error message)
         response = b""
         while True:
             data = self.request.recv(4096)
             if not data:
                 break
-            elif b"===EOF===" in data:
+            elif b"===EOF===" in data: # It's the end of the file
                 response += data.replace(b"===EOF===", b"")
                 break
             else:
                 response += data
+
+        # If possible, save the file. Send a message either confirming file upload or error
         if response == "Error":
             self.request.sendall(b"File not found. Try again.\n")
         else:
@@ -61,6 +78,7 @@ class SFTPServerHandler(socketserver.BaseRequestHandler):
             self.request.sendall(b"File uploaded successfully.\n")
 
 if __name__ == "__main__":
+    # Start the server, and verify the host and port of the server
     host, port = "192.168.8.135", 2222
     server = socketserver.TCPServer((host, port), SFTPServerHandler)
     print(f"SFTP server is running on {host}:{port}")
